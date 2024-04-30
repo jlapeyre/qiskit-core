ec3ade252ee522aeef3e25d063d7ee81a1fca2ae
John Lapeyre <jlapeyre@users.noreply.github.com>
2024-04-30 18:09:10 -0400

    Implement explicit code for kron(identity, mat) 2x2 inputs

--- crates/accelerate/src/convert_2q_block_matrix.rs
+++ crates/accelerate/src/convert_2q_block_matrix.rs
@@ -14,16 +14,33 @@ use pyo3::prelude::*;
 use pyo3::wrap_pyfunction;
 use pyo3::Python;
 
+use ndarray::array;
 use num_complex::Complex64;
-use numpy::ndarray::linalg::kron;
-use numpy::ndarray::{aview2, Array2, ArrayView2};
+use numpy::ndarray::{Array2, ArrayView2};
 use numpy::{IntoPyArray, PyArray2, PyReadonlyArray2};
 use smallvec::SmallVec;
 
-static ONE_QUBIT_IDENTITY: [[Complex64; 2]; 2] = [
-    [Complex64::new(1., 0.), Complex64::new(0., 0.)],
-    [Complex64::new(0., 0.), Complex64::new(1., 0.)],
-];
+// Compute `kron(identity, mat)` for 2x2 matrix inputs
+fn kron_id2_oneq(oneq_mat: ArrayView2<Complex64>) -> Array2<Complex64> {
+    let _zero = Complex64::new(0., 0.);
+    array![
+        [oneq_mat[[0, 0]], oneq_mat[[0, 1]], _zero, _zero],
+        [oneq_mat[[1, 0]], oneq_mat[[1, 1]], _zero, _zero],
+        [_zero, _zero, oneq_mat[[0, 0]], oneq_mat[[0, 1]]],
+        [_zero, _zero, oneq_mat[[1, 0]], oneq_mat[[1, 1]]],
+    ]
+}
+
+// Compute `kron(mat, identity)` for 2x2 matrix inputs
+fn kron_oneq_id2(oneq_mat: ArrayView2<Complex64>) -> Array2<Complex64> {
+    let _zero = Complex64::new(0., 0.);
+    array![
+        [oneq_mat[[0, 0]], _zero, oneq_mat[[0, 1]], _zero],
+        [_zero, oneq_mat[[0, 0]], _zero, oneq_mat[[0, 1]]],
+        [oneq_mat[[1, 0]], _zero, oneq_mat[[1, 1]], _zero],
+        [_zero, oneq_mat[[1, 0]], _zero, oneq_mat[[1, 1]]],
+    ]
+}
 
 /// Return the matrix Operator resulting from a block of Instructions.
 #[pyfunction]
@@ -32,11 +49,10 @@ pub fn blocks_to_matrix(
     py: Python,
     op_list: Vec<(PyReadonlyArray2<Complex64>, SmallVec<[u8; 2]>)>,
 ) -> PyResult<Py<PyArray2<Complex64>>> {
-    let identity = aview2(&ONE_QUBIT_IDENTITY);
     let input_matrix = op_list[0].0.as_array();
     let mut matrix: Array2<Complex64> = match op_list[0].1.as_slice() {
-        [0] => kron(&identity, &input_matrix),
-        [1] => kron(&input_matrix, &identity),
+        [0] => kron_id2_oneq(input_matrix),
+        [1] => kron_oneq_id2(input_matrix),
         [0, 1] => input_matrix.to_owned(),
         [1, 0] => change_basis(input_matrix),
         [] => Array2::eye(4),
@@ -46,8 +62,8 @@ pub fn blocks_to_matrix(
         let op_matrix = op_matrix.as_array();
 
         let result = match q_list.as_slice() {
-            [0] => Some(kron(&identity, &op_matrix)),
-            [1] => Some(kron(&op_matrix, &identity)),
+            [0] => Some(kron_id2_oneq(op_matrix)),
+            [1] => Some(kron_oneq_id2(op_matrix)),
             [1, 0] => Some(change_basis(op_matrix)),
             [] => Some(Array2::eye(4)),
             _ => None,
